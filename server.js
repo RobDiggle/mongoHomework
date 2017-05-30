324459feec76468e1fca161d40281917a45b9ff7
Robert Diggle <Robschach13@yahoo.com>
2017-05-30 19:41:45 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+var request = require('request');
+var cheerio = require('cheerio');
+var urls = [];
+
+request('https://www.reddit.com/r/badphilosophy/', function(err, resp, body){
+	if(!err && resp.statusCode == 200){
+		var $ = cheerio.load(body);
+		$('a.title', '#siteTable').each(function(){
+			var url = $(this).attr('href');
+			urls.push(url);
+		});
+
+		document.write(urls);
+		console.log(urls);
+	}
+
+
+});
+
+module.exports = urls;
